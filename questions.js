4289dc6d7a64655fc540e374fa30a61f83990fd6
RonaldM- <ronald.marcel.mr@gmail.com>
2016-07-18 01:45:33 +0200

    exo 13

--- questions.js
+++ questions.js
@@ -1,45 +1,126 @@
 var selectElementsStartingWithA = function(array) {
-    return 'Write your method here';
+    var tab_A = [];
+        for (var i = 0; i < array.length; i++) {
+            var fruit = array[i];
+            console.log(fruit)
+            if(fruit.startsWith("a")){
+                tab_A.push(fruit);
+            }
+    }
+    return tab_A;
 }
 
 var selectElementsStartingWithVowel = function(array) {
-    return 'Write your method here';
+    var tab_B = [];
+        for (var i = 0; i < array.length; i++) {
+            var name = array[i];
+            console.log(name);
+            if(name.startsWith("o") || name.startsWith("i") || name.startsWith("a")){
+                tab_B.push(name);
+            }
+    }
+    return tab_B;
 }
 
 var removeNullElements = function(array) {
-    return 'Write your method here';
+    var tab_C = [];
+        for (var i = 0; i < array.length; i++) {
+            var type = array[i];
+            console.log(name);
+            if(type != null){
+                tab_C.push(type);
+                console.log(tab_C);
+            }
+    }
+    return tab_C;
 }
 
 var removeNullAndFalseElements = function(array) {
-    return 'Write your method here';
+    var tab_D = [];
+        for (var i = 0; i < array.length; i++) {
+            var type = array[i];
+            console.log(type);
+            if(type != null && type !== false){
+                tab_D.push(type);
+                console.log(tab_D);
+            }
+    }
+    return tab_D;
 }
 
 var reverseWordsInArray = function(array) {
-    return 'Write your method here';
+    var tab_reverse = [];
+        for (var i=0; i<array.length; i++){
+            var word_reverse= array[i].split("").reverse().join("");
+            console.log(word_reverse);
+            tab_reverse.push(word_reverse);
+        }
+    return tab_reverse;
 }
 
 var everyPossiblePair = function(array) {
-    return 'Write your method here';
+
+  var result = [];
+    for (var i = 0 ; i < array.length ; i++) {
+        var paire = [];
+        paire.push(array[i]);
+        if (i == array.length-1) {      
+            paire.push(array[0]);
+        }
+        else { 
+            paire.push(array[i+1]);
+        }
+        result.push(paire.sort());
+    }
+    return result.sort();
 }
 
 var allElementsExceptFirstThree = function(array) {
-    return 'Write your method here';
+    array.splice(0,3);
+    return array;
 }
 
 var addElementToBeginning = function(array, element) {
-    return 'Write your method here';
+    array.unshift(element);
+    return array;
 }
 
 var sortByLastLetter = function(array) {
-    return 'Write your method here';
+    var result=[];
+
+    for (var i = 0; i < array.length; i++) {
+        var retourner = array[i].split("").reverse().join(""); 
+        result.push(retourner);
+    }
+    console.log(result);
+    result.sort();
+    array = result;
+    console.log(array);
+        result=[];
+    for (var i = 0; i < array.length; i++) {
+        var bonsens = array[i].split("").reverse().join("");
+            console.log(bonsens);
+            result.push(bonsens);
+        }
+
+return result;
 }
 
 var getFirstHalf = function(string) {
-    return 'Write your method here';
+    // Math.ceil converti en nombre entier
+    var moitie = Math.round(string.length/2);
+    console.log(moitie);
+    var str = string.slice(0,moitie);
+    console.log(str);
+    return str;
+    
 }
 
 var makeNegative = function(number) {
-    return 'Write your method here';
+    if (number > 0) {
+    number = -1 * number
+    }
+    return number;
 }
 
 var numberOfPalindromes = function(array) {
@@ -47,11 +128,26 @@ var numberOfPalindromes = function(array) {
 }
 
 var shortestWord = function(array) {
-    return 'Write your method here';
+    result =array[0];
+    for (var i = 0; i < array.length; i++) {
+        if(result.length > array[i].length){
+            result = array[i];
+            console.log(result);
+        }
+    }
+    return result;
 }
 
 var longestWord = function(array) {
-    return 'Write your method here';
+    result = array[0];
+    console.log(result);
+    for (var i = 0; i < array.length; i++) {
+        if(result.length < array[i].length){
+            result = array[i];
+            console.log(result);
+        }
+    }
+    return result;
 }
 
 var sumNumbers = function(array) {
@@ -77,7 +173,7 @@ var getElementsUntilGreaterThanFive = function(array) {
 var convertArrayToObject = function(array) {
     return 'Write your method here';
 }
-
+/////////////////////////////////////////////
 var getAllLetters = function(array) {
     return 'Write your method here';
 }
